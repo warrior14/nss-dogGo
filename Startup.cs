3f97402c66bd3f15206680fa857274690fc50abc
warrior14 <ironaddicts14@gmail.com>
2021-09-20 11:43:31 -0500

    added a WalkerRepository and IWalkerRepository.cs

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DogGo.Repositories;
+
 
 namespace DogGo
 {
@@ -24,6 +26,8 @@ namespace DogGo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<IWalkerRepository, WalkerRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
