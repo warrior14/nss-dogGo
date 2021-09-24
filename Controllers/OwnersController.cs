bf7f9722405bce8327351474ff2b058048e8e088
warrior14 <ironaddicts14@gmail.com>
2021-09-24 10:34:41 -0500

    created a LoginViewModel.cs and also made methods that corresponds to the file

--- Controllers/OwnersController.cs
+++ Controllers/OwnersController.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using DogGo.Repositories;
 using DogGo.Models;
 using DogGo.Models.ViewModels;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 
 namespace DogGo.Controllers
 {
@@ -114,6 +117,8 @@ namespace DogGo.Controllers
             }
         }
 
+
+
         // GET: Owners/Delete/5
         public ActionResult Delete(int id)
         {
@@ -136,5 +141,53 @@ namespace DogGo.Controllers
                 return View(owner);
             }
         }
+
+
+
+        public ActionResult Login()
+                {
+                    return View();
+                }
+
+                [HttpPost]
+                public async Task<ActionResult> Login(LoginViewModel viewModel)
+                {
+                    Owner owner = _ownerRepo.GetOwnerByEmail(viewModel.Email);
+
+                    if (owner == null)
+                    {
+                        return Unauthorized();
+                    }
+
+                    List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, owner.Id.ToString()),
+                new Claim(ClaimTypes.Email, owner.Email),
+                new Claim(ClaimTypes.Role, "DogOwner"),
+            };
+
+                    ClaimsIdentity claimsIdentity = new ClaimsIdentity(
+                        claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    await HttpContext.SignInAsync(
+                        CookieAuthenticationDefaults.AuthenticationScheme,
+                        new ClaimsPrincipal(claimsIdentity));
+
+                    return RedirectToAction("Index", "Dogs");
+                }
+
+                public async Task<ActionResult> Logout()
+                {
+                    await HttpContext.SignOutAsync();
+                    return RedirectToAction("Index", "Home");
+                }
+
+
+
+
+
+
+
+
     }
 }
